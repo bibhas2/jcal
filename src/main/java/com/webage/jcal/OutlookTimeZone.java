80d210535c40527b75f693cecfac79c4443b4731
Bibhas Bhattacharya <bibhas2@gmail.com>
2024-07-12 11:06:55 -0400

    Map IANA time zone ID

--- src/main/java/com/webage/jcal/OutlookTimeZone.java
+++ src/main/java/com/webage/jcal/OutlookTimeZone.java
@@ -1,22 +1,26 @@
 package com.webage.jcal;
 
 import java.util.HashMap;
+import java.util.Optional;
 
 /**
     * These are non-standard time zone IDs used by Windows
-    * and Outlook. A full list is available from Windows
-    * registry key: HKEY_LOCAL_MACHINE\Software\Microsoft\Windows NT\CurrentVersion\Time Zones
+    * and Outlook. A full list is available from:
+    * https://learn.microsoft.com/en-us/windows-hardware/manufacture/desktop/default-time-zones?view=windows-11
+    *
+    * For maximum compatibility with Outlook try to use these time zones.
     */
 public enum OutlookTimeZone {
     TZ_ROMANCE_STANDARD_TIME("Romance Standard Time"),
     TZ_SINGAPORE_STANDARD_TIME("Singapore Standard Time"),
     TZ_INDIA_STANDARD_TIME("India Standard Time"),
-    TZ_HELSINKI_STANDARD_TIME("FLE Standard Time"),
+    TZ_FLE_STANDARD_TIME("FLE Standard Time"),
     TZ_GMT_STANDARD_TIME("GMT Standard Time");
 
     private final String tzId;
 
     private static final HashMap<String, String> vTimeZoneMap;
+    private static final HashMap<String, OutlookTimeZone> ianaTzIdMap;
 
     static {
         vTimeZoneMap = new HashMap<>();
@@ -37,6 +41,69 @@ public enum OutlookTimeZone {
             "RRULE:FREQ=YEARLY;INTERVAL=1;BYDAY=-1SU;BYMONTH=3\r\n" + 
             "END:DAYLIGHT\r\n" + 
             "END:VTIMEZONE\r\n");
+
+        vTimeZoneMap.put(TZ_SINGAPORE_STANDARD_TIME.toString(), 
+            "BEGIN:VTIMEZONE\r\n" + 
+            "TZID:Singapore Standard Time\r\n" + 
+            "BEGIN:STANDARD\r\n" + 
+            "DTSTART:16010101T000000\r\n" + 
+            "TZOFFSETFROM:+0800\r\n" + 
+            "TZOFFSETTO:+0800\r\n" + 
+            "END:STANDARD\r\n" + 
+            "END:VTIMEZONE\r\n");
+
+        vTimeZoneMap.put(TZ_FLE_STANDARD_TIME.toString(), 
+            "BEGIN:VTIMEZONE\r\n" + 
+            "TZID:FLE Standard Time\r\n" + 
+            "BEGIN:STANDARD\r\n" + 
+            "DTSTART:16011028T040000\r\n" + 
+            "RRULE:FREQ=YEARLY;BYDAY=-1SU;BYMONTH=10\r\n" + 
+            "TZOFFSETFROM:+0300\r\n" + 
+            "TZOFFSETTO:+0200\r\n" + 
+            "END:STANDARD\r\n" + 
+            "BEGIN:DAYLIGHT\r\n" + 
+            "DTSTART:16010325T030000\r\n" + 
+            "RRULE:FREQ=YEARLY;BYDAY=-1SU;BYMONTH=3\r\n" + 
+            "TZOFFSETFROM:+0200\r\n" + 
+            "TZOFFSETTO:+0300\r\n" + 
+            "END:DAYLIGHT\r\n" + 
+            "END:VTIMEZONE\r\n");
+
+        vTimeZoneMap.put(TZ_GMT_STANDARD_TIME.toString(), 
+            "BEGIN:VTIMEZONE\r\n" + 
+            "TZID:GMT Standard Time\r\n" + 
+            "BEGIN:STANDARD\r\n" + 
+            "DTSTART:16011028T020000\r\n" + 
+            "RRULE:FREQ=YEARLY;BYDAY=-1SU;BYMONTH=10\r\n" + 
+            "TZOFFSETFROM:+0100\r\n" + 
+            "TZOFFSETTO:-0000\r\n" + 
+            "END:STANDARD\r\n" + 
+            "BEGIN:DAYLIGHT\r\n" + 
+            "DTSTART:16010325T010000\r\n" + 
+            "RRULE:FREQ=YEARLY;BYDAY=-1SU;BYMONTH=3\r\n" + 
+            "TZOFFSETFROM:-0000\r\n" + 
+            "TZOFFSETTO:+0100\r\n" + 
+            "END:DAYLIGHT\r\n" + 
+            "END:VTIMEZONE\r\n");
+
+        vTimeZoneMap.put(TZ_INDIA_STANDARD_TIME.toString(), 
+            "BEGIN:VTIMEZONE\r\n" + 
+            "TZID:India Standard Time\r\n" + 
+            "BEGIN:STANDARD\r\n" + 
+            "DTSTART:16010101T000000\r\n" + 
+            "TZOFFSETFROM:+0530\r\n" + 
+            "TZOFFSETTO:+0530\r\n" + 
+            "END:STANDARD\r\n" + 
+            "END:VTIMEZONE\r\n");
+
+
+        ianaTzIdMap = new HashMap<>();
+
+        ianaTzIdMap.put("Asia/Singapore", TZ_SINGAPORE_STANDARD_TIME);
+        ianaTzIdMap.put("Asia/Kolkata", TZ_INDIA_STANDARD_TIME);
+        ianaTzIdMap.put("Europe/Lisbon", TZ_GMT_STANDARD_TIME);
+        ianaTzIdMap.put("Europe/Paris", TZ_ROMANCE_STANDARD_TIME);
+        ianaTzIdMap.put("Europe/Helsinki", TZ_FLE_STANDARD_TIME);
     }
 
     private OutlookTimeZone(String tzId) {
@@ -54,5 +121,18 @@ public enum OutlookTimeZone {
      */
     public String toVTimeZone() {
         return vTimeZoneMap.get(tzId);
+    }
+
+    /**
+     * Converts a IANA time zone ID to OutlookTimeZone.
+     * A list of IANA ids can be found at:
+     * 
+     * https://en.wikipedia.org/wiki/List_of_tz_database_time_zones
+     * 
+     * @param tzId IANA time zone id. Such as "Europe/Lisbon"
+     * @return The matching OutlookTimeZone.
+     */
+    public static Optional<OutlookTimeZone> fromIANATimeZone(String tzId) {
+        return Optional.ofNullable(ianaTzIdMap.get(tzId));
     }
 }
